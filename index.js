407c40d12ac79a7eb81afc955d3600713de261c5
Jonatan Sanchez <jonatansan1@hotmail.com>
2025-03-19 22:36:36 +0100

    new commit

--- index.js
+++ index.js
@@ -8,21 +8,18 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const corsOptions = {
-  origin: '*',
+  origin: 'https://eco-front-nine.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 
+app.use(cors(corsOptions));
 connectDB();
 connectCloudinary();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permite cualquier dominio
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
-});
+
 app.use('/api/v1/ecoturismo', mainRouter);
 
 app.use('*', ({ res }) => {
