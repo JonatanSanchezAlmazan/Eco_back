47f63cc5214e0f76ebe59f1fd2c617ca63661a97
Jonatan Sanchez <jonatansan1@hotmail.com>
2025-03-19 23:03:24 +0100

    new commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 const corsOptions = {
   origin: 'https://eco-front-nine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 
