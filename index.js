dd63d2fffb9703c9923e5518bc4a69b0584623f3
Jonatan Sanchez <jonatansan1@hotmail.com>
2025-03-19 22:21:28 +0100

    new commit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const corsOptions = {
-  origin: 'https://eco-front-nine.vercel.app', // O un dominio específico si prefieres restringir los orígenes
+  origin: 'https://eco-front-nine.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Permite el envío de cookies
+  credentials: true
 };
 
 connectDB();
