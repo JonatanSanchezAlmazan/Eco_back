d6f2ca7364f2fa41194ca6e472174738c3467614
Jonatan Sanchez <jonatansan1@hotmail.com>
2025-03-19 22:34:09 +0100

    new commit

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ connectCloudinary();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permite cualquier dominio
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
 app.use('/api/v1/ecoturismo', mainRouter);
 
 app.use('*', ({ res }) => {
